10d9d13d61aa364c15b0ca8a484969f145cf3c90
Ronny Araujo <ronnyaraujo57@gmail.com>
2021-01-03 15:37:21 -0400

    cambios menores

--- public/tarea_9/js/script.js
+++ public/tarea_9/js/script.js
@@ -1,6 +1,7 @@
-const tabBody = document.querySelectorAll(".container .tabBody") 
-const form = document.getElementById("formulario")
-const indicador = document.querySelectorAll(".indicador")
+const tabBody = document.querySelectorAll(".container .tabBody");
+const form = document.getElementById("formulario");
+const indicador = document.querySelectorAll(".indicador");
+const inputs = document.getElementsByTagName('input');
 
 function tab(index, direccion) {
 	if(direccion === "next" ){
@@ -18,5 +19,26 @@ function tab(index, direccion) {
 	}else
 		tabBody[index].style.display = 'none';
 
+}
+
+function agregar_fila(){
+	let count = inputs.length;
+	var tablaDinamica = document.querySelector("#tablaDinamica");
+	console.log(`nueva fila (elementos ${count})`);
+	for (var uno in inputs) {
+		console.log("primer nivel for");
+		if (uno.id === 'datosPersonales') {
+			var fila = document.createElement("tr");
+			for (var j = 0; j < count; i++) {
+				console.log("segundo nivel for");
+				var celda = document.createElement("td");
+				var textCelda = document.createTextNode(uno.value);
+
+				celda.appendChild(textCelda);
+				fila.appendChild(celda);
+			}
+			tablaDinamica.appendChild(fila);
+		}
+	}
 }
 
